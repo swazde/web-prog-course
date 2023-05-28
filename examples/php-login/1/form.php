e77c815063e74bd9110d0a63968f0809f4b1b04a
WengHowe Chan <49468232+cwenghowe@users.noreply.github.com>
2023-05-28 09:05:46 +0800

    Add files via upload

---
+++ examples/php-login/1/form.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Login with session without database</title>
+</head>
+<body>
+<form method="post" action="login.php">
+<p>Username: <input type="text" name="username" /></p>
+<p>Password: <input type="password" name="password"/></p>
+<p><input type="submit" value="Let me in" /></p>
+</form>
+</body>
+</html>
